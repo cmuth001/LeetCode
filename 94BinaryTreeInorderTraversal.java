2073a41cb419f21f7a2d4f7fda9baaedec401561
Chandrasekhar Reddy Muthyala <cmuth001@odu.edu>
2020-04-07 23:30:23 -0400

    BinaryTreeInorderTraversal

---
+++ 94BinaryTreeInorderTraversal.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class 94BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inOrderList=new ArrayList<Integer>();
+        getInorderList(root, inOrderList);
+        return inOrderList;
+        
+    }
+    public void getInorderList(TreeNode root, List<Integer> inOrderList){
+        if(root == null){
+            return;
+        }
+    getInorderList(root.left, inOrderList);
+    inOrderList.add(root.val);
+     getInorderList(root.right, inOrderList);
+    }
+}
