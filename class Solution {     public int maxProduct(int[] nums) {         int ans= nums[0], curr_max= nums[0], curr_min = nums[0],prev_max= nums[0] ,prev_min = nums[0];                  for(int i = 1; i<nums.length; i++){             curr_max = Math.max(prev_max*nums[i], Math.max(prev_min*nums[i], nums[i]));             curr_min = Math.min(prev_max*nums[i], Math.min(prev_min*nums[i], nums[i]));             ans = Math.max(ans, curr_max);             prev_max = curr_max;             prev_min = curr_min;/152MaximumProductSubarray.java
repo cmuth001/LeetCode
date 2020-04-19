b2963b5758de6738cc3cbcd41357e16497ee1768
Chandrasekhar Reddy Muthyala <cmuth001@odu.edu>
2020-04-19 15:52:25 -0400

    152MaximumProductSubarray

---
+++ class Solution {     public int maxProduct(int[] nums) {         int ans= nums[0], curr_max= nums[0], curr_min = nums[0],prev_max= nums[0] ,prev_min = nums[0];                  for(int i = 1; i<nums.length; i++){             curr_max = Math.max(prev_max*nums[i], Math.max(prev_min*nums[i], nums[i]));             curr_min = Math.min(prev_max*nums[i], Math.min(prev_min*nums[i], nums[i]));             ans = Math.max(ans, curr_max);             prev_max = curr_max;             prev_min = curr_min;/152MaximumProductSubarray.java
@@ -0,0 +1,16 @@
+   
+class 152MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        int ans= nums[0], curr_max= nums[0], curr_min = nums[0],prev_max= nums[0] ,prev_min = nums[0];
+        
+        for(int i = 1; i<nums.length; i++){
+            curr_max = Math.max(prev_max*nums[i], Math.max(prev_min*nums[i], nums[i]));
+            curr_min = Math.min(prev_max*nums[i], Math.min(prev_min*nums[i], nums[i]));
+            ans = Math.max(ans, curr_max);
+            prev_max = curr_max;
+            prev_min = curr_min;
+            // System.out.println(curr_max+","+ curr_min+","+ ans+","+prev_max +","+prev_min);
+        }
+        return ans;
+    }
+}
