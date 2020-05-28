8ed40e1b7f38b95ca0b8f2b9779fd4ab03850aa0
Chandrasekhar Reddy Muthyala <cmuth001@odu.edu>
2020-05-28 05:08:32 -0400

    Update 94inorderTraversal.java

--- 94inorderTraversal.java
+++ 94inorderTraversal.java
@@ -25,3 +25,22 @@ class Solution {
         getInorderList(root.right, inOrderList); 
     }
 }
+
+
+// Method-2:
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inOrderList = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            inOrderList.add(root.val);
+            root = root.right;            
+        }
+        return inOrderList;
+    }
+}
