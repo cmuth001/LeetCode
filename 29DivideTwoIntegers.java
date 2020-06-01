d9dca6940e9eec7b3fef0f6eca47ff5573d2815d
Chandrasekhar Reddy Muthyala <cmuth001@odu.edu>
2020-06-01 02:12:30 -0400

    Update 29DivideTwoIntegers.java

--- 29DivideTwoIntegers.java
+++ 29DivideTwoIntegers.java
@@ -17,3 +17,29 @@ class Solution {
         
     }
 }
+
+// 2nd method-maximum divisor power
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor==-1){
+            return Integer.MAX_VALUE;
+        }
+        
+        int sign = (dividend<0) ^ (divisor<0)?-1:1;
+        int result = 0;
+        
+        long x = Math.abs((long)dividend);
+        long y = Math.abs((long)divisor);
+        
+        while(x-y>=0){
+            int count = 0;
+            while(x-(y<<1<<count)>=0){
+                count++;
+            }
+            result += 1<<count;
+            x = x - (y<<count);
+        }
+        return result*sign;
+        
+    }
+}
