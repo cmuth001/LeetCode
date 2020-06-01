68b8e3e3d374b9edcb0f65f2783bfa44c964a41a
Chandrasekhar Reddy Muthyala <cmuth001@odu.edu>
2020-05-31 21:42:24 -0400

    Update 98isValidBST.java

--- 98isValidBST.java
+++ 98isValidBST.java
@@ -33,3 +33,33 @@ class Solution {
         
     }
 }
+
+// 2nd method
+class Solution {
+    LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+    LinkedList<Integer> uppers = new LinkedList<Integer>();
+    LinkedList<Integer> lowers = new LinkedList<Integer>();
+    public void updateStack(TreeNode node, Integer lower, Integer upper){
+        stack.add(node);
+        lowers.add(lower);
+        uppers.add(upper);
+    }
+    public boolean isValidBST(TreeNode root) {
+        Integer upper = null, lower = null, val;
+        updateStack(root, lower, upper);
+        while(!stack.isEmpty()){
+            root = stack.poll();
+            lower = lowers.poll();
+            upper = uppers.poll();
+            if(root == null) continue;
+            val = root.val;
+            if(lower != null && lower>=val) return false;
+            if(upper != null && upper <= val) return false;
+            
+            updateStack(root.left, lower, val);
+            updateStack(root.right, val, upper);
+        }
+        return true;
+        
+    }
+}
