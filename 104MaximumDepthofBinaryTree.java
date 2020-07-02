de0f682e3db4d6f58538903022ceffc11b1f10b3
Chandrasekhar Reddy Muthyala <cmuth001@odu.edu>
2020-07-02 01:06:21 -0400

    Update 104MaximumDepthofBinaryTree.java

--- 104MaximumDepthofBinaryTree.java
+++ 104MaximumDepthofBinaryTree.java
@@ -8,3 +8,22 @@ class Solution {
         return Math.max(left_depth, right_depth)+1;
     }
 }
+
+// 2nd Method
+class Solution {
+    int answer = 0;
+    public int maxDepth(TreeNode root) {
+        return answer;
+        
+    }
+    public void max_depth(TreeNode root, int depth){
+        if (root == null) return;
+        
+        if (root.left == null && root.right == null){
+            answer = Math.max(answer, depth);
+        }
+        max_depth(root.left, depth+1);
+        max_depth(root.right, depth+1);
+        
+    }
+}
